fe5b328a0d621299815085665a0bd98126a6aa9e
JosephSusik <joseph.susik@gmail.com>
2023-04-26 21:46:56 +0200

    File choose

--- src/main/java/com/example/pacman/LoadMapController.java
+++ src/main/java/com/example/pacman/LoadMapController.java
@@ -4,8 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
@@ -19,13 +23,26 @@ public class LoadMapController implements Initializable{
     @FXML
     private Button btn_back;
 
+    @FXML
+    private Label label;
+
+    public File selectedFile;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        FileChooser fil_chooser = new FileChooser();
 
         btn_selectMap.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-
+                Node node = (Node) actionEvent.getSource();
+                Stage st = (Stage) node.getScene().getWindow();
+                File file = fil_chooser.showOpenDialog(st);
+                if (file != null) {
+                    selectedFile = file;
+                    //label.setText(selectedFile.getAbsolutePath() + " selected");
+                    label.setText("File: " + selectedFile.getName());
+                }
             }
         });
 
