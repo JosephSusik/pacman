172a704a429593f91fa95b2f52e9f13ffbcff63b
JosephSusik <joseph.susik@gmail.com>
2023-04-30 21:54:25 +0200

    Pacman screen

--- src/main/java/com/example/pacman/controller/Utils.java
+++ src/main/java/com/example/pacman/controller/Utils.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,7 +23,13 @@ public class Utils {
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setTitle(title);
-        stage.setScene(new Scene(root, 320, 240));
+
+        Scene scene = new Scene(root, 320, 240);
+        scene.getStylesheets().add(Utils.class.getResource("fxml/style/styling.css").toExternalForm());
+        stage.setScene(scene);
         stage.show();
+
+        //stage.setScene(new Scene(root, 320, 240));
+        //stage.show();
     }
 }
